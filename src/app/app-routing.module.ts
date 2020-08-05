866544f7ee3ec34324dfff33036bf74546b6a42a
Rodrigo Tardelli <rodrigotmg@gmail.com>
2020-08-05 19:15:31 -0300

    Organizing form components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { SimpleFormComponent } from './simple-form/simple-form.component';
 import { FormHolderComponent } from './form-holder/form-holder.component';
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
+  { path: "simple", component: SimpleFormComponent },
   { path: "form", component: FormHolderComponent },
 ];
 
